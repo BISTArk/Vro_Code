142f37b0c05aaefeaf37ce070eb0e102cf6e99a7
BISTArk <akshaybistagond@gmail.com>
2021-09-09 20:47:32 +0530

    Login and some liberaries1

---
+++ client/src/components/App.js
@@ -0,0 +1,15 @@
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> VroCode </h1>
+        <Login/>
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
