c72effadf47989c2296e769dfbf640c46c4291c5
abhishek <chavanabhishek527@gmail.com>
2021-09-27 11:20:02 +0530

    login-updated

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,11 +1,42 @@
-import "./Login.scss";
+import "./login.css";
 
-function Login() {
-    return (
-        <div>
-            Login
+export default function Login() {
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          
+    
         </div>
-    )
+        <center>
+       <div className="loginRight">
+          
+          <div className="loginBox">
+           <h3 className="loginLogo">Create Account</h3>
+           <h1 className="vroCode">VroCode</h1>
+           <form action="">
+            <p className="input-text">Name</p>
+            <input placeholder="Enter Name" className="loginInput"/>
+            <p className="input-text">Username</p>
+            <input placeholder="Enter username" className="loginInput"/>  
+            <p className="input-text">Email address</p>  
+            <input placeholder="Enter Valid email" className="loginInput" />
+            <p className="input-text">Password</p>
+            <input placeholder="Enter a password" className="loginInput" />
+            <button className="loginButton">Log In</button>
+            <span className="alreadyMember">Already a member?</span>
+            <button className="SignUp">
+              Sign Up
+            </button>
+            </form>
+            <img src="images/login-paint-png.png" alt="vrocode-art" />
+          </div>
+        
+        </div>
+        </center>
+        
+      </div>
+      
+    </div>
+  );
 }
-
-export default Login
