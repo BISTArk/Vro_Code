11cea4e665bf82e7be51d0f97760f8d5725362fa
bdiptesh99 <bdiptesh.191mt011@nitk.edu.in>
2021-09-30 11:05:59 +0530

    Added chat components and engine

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,15 +1,15 @@
 //import _default from "@ant-design/icons/lib/icons/AccountBookFilled";
 import { ChatEngine } from "react-chat-engine";
-import Chatfeed from "./components/ChatFeed";
+import Chatfeed from "clientsrccomponentsChatChatFeed.jsx";
 import "./Chat.css";
 
 const Chat = () => {
   return (
     <ChatEngine
-    height='100vh'
-    userName='bdiptesh99'
-    userSecret='12345'
-    projectID='df5ae1a6-d0d3-4329-929b-d798827ddc4f'
+      height="100vh"
+      userName="bdiptesh99"
+      userSecret="12345"
+      projectID="df5ae1a6-d0d3-4329-929b-d798827ddc4f"
       //renderChatFeed={(chatAppProps) => <Chatfeed {...chatAppProps} />}
     />
   );
