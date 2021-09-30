e8c3e7fe14b58f6a5b68d7b97e62205cbb3e1295
abhishek <chavanabhishek527@gmail.com>
2021-09-30 19:36:38 +0530

    Commit

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -1,17 +1,25 @@
 //import _default from "@ant-design/icons/lib/icons/AccountBookFilled";
+import {React} from "react";
 import { ChatEngine } from "react-chat-engine";
 //import Chatfeed from "client/src/components/Chat/ChatFeed.jsx";
+import TopBar from "../../components/TopBar/TopBar";
 import "./Chat.css";
 
-const Chat = () => {
+const Chat = ()=>{
   return (
+   
+  
+   
+      
     <ChatEngine
       height="100vh"
-      userName="bdiptesh99"
-      userSecret="12345"
+      userName="seiphonx"
+      userSecret="Chavan@1234"
       projectID="df5ae1a6-d0d3-4329-929b-d798827ddc4f"
       //renderChatFeed={(chatAppProps) => <Chatfeed {...chatAppProps} />}
     />
+  
+   
   );
 };
 
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
     return (
         <div>
             <TopBar/>
-            <Chat />
+            
             HOME
         </div>
     )
