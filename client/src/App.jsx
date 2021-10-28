d0f827da850cb60dc43d77a8e50cadcb60eb655f
BISTArk <akshaybistagond@gmail.com>
2021-10-29 00:02:25 +0530

    works now?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,22 +14,7 @@ import Friends from "./pages/Friends/Friends";
 import Bookmark from "./pages/Bookmark/Bookmark";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
-import { useForkRef } from "@material-ui/core";
 
-{
-  /* <Switch>
-        <Route exact path="/">
-          {user ? <Home /> : <Register />}
-        </Route>
-        <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
-        <Route path="/register">
-          {user ? <Redirect to="/" /> : <Register />}
-        </Route>
-        <Route path="/profile/:username">
-          <Profile />
-        </Route>
-      </Switch> */
-}
 
 function App() {
   const { user } = useContext(AuthContext);
