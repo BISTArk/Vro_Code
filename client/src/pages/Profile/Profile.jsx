4d366cd3f362189ea9fd248729f3bcad7ff84cc4
BISTArk <akshaybistagond@gmail.com>
2021-11-19 20:51:14 +0530

    minor bug

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -33,6 +33,9 @@ export default function Profile(props) {
         `http://localhost:3030/api/post/my/${props.match.params.id}`
       );
       let jso1 = await response1.json();
+      jso1.forEach(x => {
+        x={...x,username:user.username,profilePic:user.profilePic}
+      });
       setPosts(jso1);
     }
 
