24a2331f6005536ca9a88facd1741a1f4484bd31
BISTArk <akshaybistagond@gmail.com>
2021-11-22 20:10:21 +0530

    diptesh did ooopsie

--- client/src/pages/ImageUpload/ImageUpload.jsx
+++ client/src/pages/ImageUpload/ImageUpload.jsx
@@ -4,15 +4,16 @@ import { NavLink as Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowCircleUp } from "@fortawesome/free-solid-svg-icons";
 import { AuthContext } from "../../context/AuthContext";
+import axios from"axios";
 
 class ImageUpload extends Component {
-  state = { profileImag: "", coverImag: "" };
+  state = { profileImag: "", coverImag: "", profilePic:"", coverPic:"" };
 
   onProfilePic = (e) => {
     if (e.target.files && e.target.files[0]) {
       const img = e.target.files[0];
       console.log(img);
-      this.setState({ profileImag: img });
+      this.setState({ profileImag: img, profilePic: img.name });
     }
   };
 
@@ -20,30 +21,28 @@ class ImageUpload extends Component {
     if (e.target.files && e.target.files[0]) {
       const img = e.target.files[0];
       console.log(img);
-      this.setState({ coverImag: img });
+      this.setState({ coverImag: img, coverPic:img.name });
     }
   };
 
-  sendPics = async (user,dispatch) => {
+  sendPics = async (e,user,dispatch ) => {
     const formData = new FormData();
     
-    formData.append("userid", user._id);
-    formData.append("githubLink", gitLink);
-    formData.append("content", postDesc);
-    formData.append("img", imag.name);
-    formData.append("imag", imag);
+    formData.append("coverPic", this.state.profilePic);
+    formData.append("coverImag", this.state.coverImag);
+    formData.append("profilePic", this.state.profilePic);
+    formData.append("profileImag", this.state.profileImag);
+    formData.append("id", user._id);
 
-    const response = await axios.post(
-      "http://localhost:3030/api/post",
+    const response = await axios.put(
+      "http://localhost:3030/api/user/pics",
       formData
     );
     
-    if(response.status===200)dispatch({ type: "CREATE_POST", payload: user.postCount + 1 });
-    console.log(response);
-    window.location.reload();
-
+    if(response.status===200)dispatch({ type: "UPLOAD_PICS", payload: {profilePic:response.data.profilePic,coverPic:response.data.coverPic} });
+    // console.log(response);
+    window.location.href="/profile/"+user._id;
   };
-
 
   render() {
     return (
@@ -53,7 +52,7 @@ class ImageUpload extends Component {
             <div className="imgUpload">
               <h1 className="uploadPicH1">Upload Pictures</h1>
 
-              <form action="" className="uploadForm">
+              <div className="uploadForm">
                 <div className="formElementUpload">
                   <label htmlFor="profilePic" className="labelUpload">
                     <FontAwesomeIcon
@@ -87,11 +86,11 @@ class ImageUpload extends Component {
                   />
                 </div>
                 <div className="buttonsEdit">
-                  <Link to={`/`}>
-                    <button className="cancelUpload">Cancel</button>
-                  </Link>
+                  <div>
+                    <button className="cancelUpload" onClick={(e)=>{this.sendPics(e,user,dispatch)}}>Submit</button>
+                  </div>
                 </div>
-              </form>
+              </div>
             </div>
           );
         }}
